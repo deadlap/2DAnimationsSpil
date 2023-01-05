c95001970fa0587ccec70c02f543e5a5df96db1b
deadlap <32266596+deadlap@users.noreply.github.com>
2023-01-05 14:44:30 +0100

    fikset 2. animations ting

--- IKDU_AnimationsSpil/Assets/Scripts/PlayerMovement.cs
+++ IKDU_AnimationsSpil/Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,10 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour {
 
-    private Vector2 movement;
-    private Rigidbody2D myBody;
-    private Animator myAnimator;
-
+    Vector2 movement;
+    Rigidbody2D myBody;
+    Animator myAnimator;
+    
     [SerializeField] private int speed = 5;
     
     // Start is called before the first frame update
@@ -21,27 +21,20 @@ public class PlayerMovement : MonoBehaviour {
     private void OnMovement(InputValue value){
         movement = value.Get<Vector2>();
 
-        if (movement.x != 0 && movement.x != myAnimator.GetFloat("x") && myAnimator.GetFloat("x") != 0 
-                && movement.y == 0 && myAnimator.GetFloat("y") == 0) {
-            flip();
+        if (movement.x > 0) {
+            this.transform.localScale = new Vector3(1, 1, 1);
+        } else if(movement.x < 0) {
+            this.transform.localScale = new Vector3(-1, 1, 1);
         }
 
         if(movement.x != 0 || movement.y != 0) {
-            
-            myAnimator.SetFloat("x", movement.x);
-            myAnimator.SetFloat("y", movement.y);
             myAnimator.SetBool("IsWalking", true);
         } else {
             myAnimator.SetBool("IsWalking", false);
         }
     }
-    // Update is called once per frame
-    private void FixedUpdate() {
+    
+    void FixedUpdate() {
         myBody.velocity = movement * speed;
-    }
-    private void flip(){
-        Vector3 currentScale = gameObject.transform.localScale;
-        currentScale.x *= -1;
-        this.transform.localScale = currentScale;
     }
 }
